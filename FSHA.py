e785d721552450000da7200683ba0c6e00ac6846
Dario Pasquini <mbp>
2020-12-03 09:43:15 +0100

    readme

--- FSHA.py
+++ FSHA.py
@@ -4,8 +4,6 @@ import tqdm
 import datasets, architectures, styleloss
 
 def distance_data_loss(a,b):
-    # it scales with the batch-size
-    #return tf.sqrt( tf.reduce_sum((a - b) ** 2) )
     l = tf.losses.MeanSquaredError()
     return l(a, b)
 
@@ -16,7 +14,7 @@ def distance_data(a,b):
 class FSHA:
     def __init__(self, xpriv, xpub, id_setup, batch_size, hparams):
             input_shape = xpriv.element_spec[0].shape
-            
+
             # setup dataset
             self.client_dataset = xpriv.batch(batch_size, drop_remainder=True).repeat(-1)
             self.attacker_dataset = xpub.batch(batch_size, drop_remainder=True).repeat(-1)
@@ -39,34 +37,34 @@ class FSHA:
             else:
                 self.style_model = None
             ##
-            
+
             # setup optimizer
             self.optimizer0 = tf.keras.optimizers.Adam(learning_rate=hparams['lr_f'])
             self.optimizer1 = tf.keras.optimizers.Adam(learning_rate=hparams['lr_tilde'])
             self.optimizer2 = tf.keras.optimizers.Adam(learning_rate=hparams['lr_D'])
-            
-                        
-    
+
+
+
     @staticmethod
     def addNoise(x, alpha):
         return x + tf.random.normal(x.shape) * alpha
-        
+
     @tf.function
     def train_step(self, x_private, x_public, label_private, label_public):
-    
+
         with tf.GradientTape(persistent=True) as tape:
 
             #### Virtually, ON THE CLIENT SIDE:
             # clients' smashed data
             z_private = self.f(x_private)
             ####################################
-            
-            
+
+
             #### SERVER-SIDE:
             # map to data space (for evaluation and style loss)
             rec_x_private = self.decoder(z_private)
             ## adversarial loss (f's output must similar to \tilde{f}'s output):
-            adv_private_logits = self.D(z_private)    
+            adv_private_logits = self.D(z_private)
             if self.hparams['WGAN']:
                 print("Use WGAN loss")
                 f_loss = tf.reduce_mean( tf.nn.sigmoid(adv_private_logits) )
@@ -81,13 +79,13 @@ class FSHA:
                 z_public = self.tilde_f(x_public_noise)
             else:
                 z_public = self.tilde_f(x_public)
-            
+
             # invertibility loss
             rec_x_public = self.decoder(z_public)
             public_rec_loss = distance_data_loss(x_public, rec_x_public)
             tilde_f_loss = public_rec_loss
 
-        
+
             style_loss = 0
             if self.style_model:
                 print("Regularize with style-loss")
@@ -109,13 +107,13 @@ class FSHA:
                 loss_discr_fake = tf.reduce_mean(tf.keras.losses.binary_crossentropy(tf.zeros_like(adv_private_logits), adv_private_logits, from_logits=True))
                 # discriminator's loss
                 D_loss = (loss_discr_true + loss_discr_fake) / 2
-            
+
             if 'gradient_penalty' in self.hparams:
                 print("Use GP")
                 w = float(self.hparams['gradient_penalty'])
                 D_gradient_penalty = self.gradient_penalty(z_private, z_public)
                 D_loss += D_gradient_penalty * w
-            
+
             ##################################################################
             ## attack verification #####################
             loss_c_verification = distance_data(x_private, rec_x_private)
@@ -125,23 +123,23 @@ class FSHA:
 
         # train attacker's decoder on public data
         var = self.f.trainable_variables
-        gradients = tape.gradient(f_loss, var) 
+        gradients = tape.gradient(f_loss, var)
         self.optimizer0.apply_gradients(zip(gradients, var))
 
         # train attacker's autoencoder on public data
-        var = self.tilde_f.trainable_variables + self.decoder.trainable_variables 
-        gradients = tape.gradient(tilde_f_loss, var) 
+        var = self.tilde_f.trainable_variables + self.decoder.trainable_variables
+        gradients = tape.gradient(tilde_f_loss, var)
         self.optimizer1.apply_gradients(zip(gradients, var))
 
         # desc_train
         var = self.D.trainable_variables
         gradients = tape.gradient(D_loss, var)
-        self.optimizer2.apply_gradients(zip(gradients, var))  
+        self.optimizer2.apply_gradients(zip(gradients, var))
 
 
         return f_loss, tilde_f_loss, D_loss, loss_c_verification, style_loss
-    
-    
+
+
     def gradient_penalty(self, x, x_gen):
         epsilon = tf.random.uniform([x.shape[0], 1, 1, 1], 0.0, 1.0)
         x_hat = epsilon * x + (1 - epsilon) * x_gen
@@ -152,54 +150,54 @@ class FSHA:
         ddx = tf.sqrt(tf.reduce_sum(gradients ** 2, axis=[1, 2]))
         d_regularizer = tf.reduce_mean((ddx - 1.0) ** 2)
         return d_regularizer
-    
-    
+
+
     def attack(self, x_private):
         # smashed data sent from the client:
         z_private = self.f(x_private)
         # recover private data from smashed data
         tilde_x_private = self.decoder(z_private, training=False)
-        
+
         z_private_control = self.tilde_f(x_private, training=False)
         control = self.decoder(z_private_control)
         return tilde_x_private.numpy(), control.numpy()
 
-            
+
     def __call__(self, iterations, log_frequency=500, verbose=False, progress_bar=True):
-        
+
         n = int(iterations / log_frequency)
         LOG = np.zeros((n, 5))
-        
+
         iterator = zip(self.client_dataset.take(iterations), self.attacker_dataset.take(iterations))
         if progress_bar:
             iterator = tqdm.tqdm(iterator , total=iterations)
-        
+
         i, j = 0, 0
         print("RUNNING...")
         for (x_private, label_private), (x_public, label_public) in iterator:
-            log = self.train_step(x_private, x_public, label_private, label_public)   
-            
+            log = self.train_step(x_private, x_public, label_private, label_public)
+
             if i == 0:
                 VAL = log[3]
             else:
                 VAL += log[3] / log_frequency
-                
+
             if  i % log_frequency == 0:
                 LOG[j] = log
-                
+
                 if verbose:
                     print("log--%02d%%-%07d] validation: %0.4f" % ( int(i/iterations*100) ,i, VAL) )
-                
+
                 VAL = 0
                 j += 1
-                
-                
+
+
             i += 1
         return LOG
 
 
-#----------------------------------------------------------------------------------------------------------------------    
-    
+#----------------------------------------------------------------------------------------------------------------------
+
 def binary_accuracy(label, logits):
     p = tf.nn.sigmoid(logits)
     predicted = tf.cast( (p > .5), tf.float32)
@@ -209,29 +207,24 @@ def binary_accuracy(label, logits):
 def classification_loss(label, logits):
     return tf.reduce_mean(tf.keras.losses.binary_crossentropy(label, logits, from_logits=True))
 
-def distance_data(a,b):
-    #return tf.reduce_mean(tf.keras.losses.mean_squared_error(a,b))
-    return tf.reduce_mean( tf.sqrt( tf.reduce_sum((a - b) ** 2, 1) ) )
-
-
 
 class FSHA_binary_property(FSHA):
-   
+
     @tf.function
     def train_step(self, x_private, x_public, label_private, label_public):
-    
+
         with tf.GradientTape(persistent=True) as tape:
 
             #### Virtually, ON THE CLIENT SIDE:
             # clients' smashed data
             z_private = self.f(x_private)
             ####################################
-            
+
             #### SERVER-SIDE:
             # map to data space (for evaluation and style loss)
             clss_private_logits = self.decoder(z_private)
             ## adversarial loss (f's output must similar to \tilde{f}'s output):
-            adv_private_logits = self.D(z_private)    
+            adv_private_logits = self.D(z_private)
             if self.hparams['WGAN']:
                 print("Use WGAN loss")
                 f_loss = tf.reduce_mean( tf.nn.sigmoid(adv_private_logits) )
@@ -242,7 +235,7 @@ class FSHA_binary_property(FSHA):
             # attacker's classifier
             z_public = self.tilde_f(x_public)
             clss_public_logits = self.decoder(z_public)
-            
+
             # classificatio loss
             public_classification_loss = classification_loss(label_public, clss_public_logits)
             public_classification_accuracy = binary_accuracy(label_public, clss_public_logits)
@@ -262,13 +255,13 @@ class FSHA_binary_property(FSHA):
                 loss_discr_fake = tf.reduce_mean(tf.keras.losses.binary_crossentropy(tf.zeros_like(adv_private_logits), adv_private_logits, from_logits=True))
                 # discriminator's loss
                 D_loss = (loss_discr_true + loss_discr_fake) / 2
-            
+
             if 'gradient_penalty' in self.hparams:
                 print("Use GP")
                 w = float(self.hparams['gradient_penalty'])
                 D_gradient_penalty = self.gradient_penalty(z_private, z_public)
                 D_loss += D_gradient_penalty * w
-            
+
             ##################################################################
             ## attack verification #####################
             private_classification_accuracy = binary_accuracy(label_private, clss_private_logits)
@@ -278,18 +271,18 @@ class FSHA_binary_property(FSHA):
 
         # train attacker's decoder on public data
         var = self.f.trainable_variables
-        gradients = tape.gradient(f_loss, var) 
+        gradients = tape.gradient(f_loss, var)
         self.optimizer0.apply_gradients(zip(gradients, var))
 
         # train attacker's autoencoder on public data
-        var = self.tilde_f.trainable_variables + self.decoder.trainable_variables 
-        gradients = tape.gradient(tilde_f_loss, var) 
+        var = self.tilde_f.trainable_variables + self.decoder.trainable_variables
+        gradients = tape.gradient(tilde_f_loss, var)
         self.optimizer1.apply_gradients(zip(gradients, var))
 
         # desc_train
         var = self.D.trainable_variables
         gradients = tape.gradient(D_loss, var)
-        self.optimizer2.apply_gradients(zip(gradients, var))  
+        self.optimizer2.apply_gradients(zip(gradients, var))
 
 
         return f_loss, tilde_f_loss, D_loss, private_classification_accuracy, public_classification_accuracy
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# Unleashing the Tiger: Inference Attacks on Split Learning ⚡️🐯⚡️
+# Unleashing the Tiger: Inference Attacks on Split Learning 🐯
 We investigate the security of **split learning**---a novel collaborative machine learning framework that enables peak performance by requiring minimal resources consumption. In the paper, we make explicit the vulnerabilities of the protocol and demonstrate its inherent insecurity by introducing general attack strategies targeting the reconstruction of clients' private training sets. More prominently, we demonstrate that a malicious server can actively hijack the learning process of the distributed model and bring it into an insecure state that enables inference attacks on clients' data. We implement different adaptations of the attack and test them on various datasets as well as within realistic threat scenarios.
+
+## Code:
+
+This repository contains all the code needed to run the **Feature-space hijacking attack** and its variations.The implementation is based on **TensorFlow2**.
+
+In particular:
+
+*  *FSHA.py*: It implements the attack and a single-user version of split learning. The class *FSHA_binary_property* in the file implements the property-inference attack.
+* *architectures.py*: It contains the main network architectures we used in the paper.
+* *styleloss.py*: It implements the style loss based on a MobileNet trained on *ImageNet*.
+* *datasets.py*: It contains utility to load and parse datasets.
+
+## Proof Of Concept:
+
+We report a set of *jupyter notebooks* that act as brief tutorial for the code and replicate the experiments in the paper. Those are:
+
+* *FSHA.ipynb*: It implements the standard Feature-space hijacking attack on the MNIST dataset.
+* *FSHA_mangled.ipynb*: It implements the Feature-space hijacking attack on the MNIST dataset, when the attacker's training set is mangled of a class.
+* Work in progress.....
+
+## Cite our work:
+
+> 
 
